2f42be370a4e48e3ea09e42a730ced1da7935b22
Mehmet Adil Özsakız <mehmetadilozsakiz@gmail.com>
2022-08-04 16:03:18 +0300

    some changes on appuser and registration is working fine, user can be saved on database

--- src/main/java/com/example/userlogin/appuser/AppUserRepository.java
+++ src/main/java/com/example/userlogin/appuser/AppUserRepository.java
@@ -1,11 +1,12 @@
 package com.example.userlogin.appuser;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Transactional(readOnly = true)
-public interface AppUserReposiyory {
+public interface AppUserRepository extends JpaRepository<AppUser,Long> {
 
     Optional<AppUser> findByEmail(String email);
 }
