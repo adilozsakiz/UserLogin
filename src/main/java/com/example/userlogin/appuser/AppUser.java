ef42d07b6bcbcb5cbe8fb7f17885c91382400d5f
Mehmet Adil Özsakız <mehmetadilozsakiz@gmail.com>
2022-08-10 14:33:20 +0300

    we can send confirmation mail and confirm user registration

--- src/main/java/com/example/userlogin/appuser/AppUser.java
+++ src/main/java/com/example/userlogin/appuser/AppUser.java
@@ -18,12 +18,12 @@ import java.util.Collections;
 @NoArgsConstructor
 @Entity
 public class AppUser implements UserDetails {
-    @Id
     @SequenceGenerator(
             name = "student_sequence",
             sequenceName = "student_sequence",
             allocationSize = 1
     )
+    @Id
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
             generator = "student_sequence"
--- src/main/java/com/example/userlogin/appuser/AppUserService.java
+++ src/main/java/com/example/userlogin/appuser/AppUserService.java
@@ -1,11 +1,16 @@
 package com.example.userlogin.appuser;
 
+import com.example.userlogin.registration.token.ConfirmationToken;
+import com.example.userlogin.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -16,6 +21,7 @@ public class AppUserService implements UserDetailsService {
 
     private final AppUserRepository appUserRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
 
     @Override
     public UserDetails loadUserByUsername(String email)
@@ -24,20 +30,33 @@ public class AppUserService implements UserDetailsService {
                 .orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG,email)));
     }
 
-    public String singUpUser(AppUser appUser){
+    public String signUpUser(AppUser appUser){
         boolean userExists = appUserRepository.findByEmail(appUser.getEmail())
                 .isPresent();
         if(userExists){
             throw new IllegalStateException("email already taken");
         }
-        String encoddedPassword = bCryptPasswordEncoder
+        String encodedPassword = bCryptPasswordEncoder
                 .encode(appUser.getPassword());
 
-        appUser.setPassword(encoddedPassword);
+        appUser.setPassword(encodedPassword);
         appUserRepository.save(appUser);
 
-        // TODO: send confirmation token
+        String token = UUID.randomUUID().toString();
 
-        return "it works";
+                ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                    appUser
+        );
+
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+        return token;
+    }
+
+    public int enableAppUser(String email) {
+        
+        return appUserRepository.enableAppUser(email);
     }
 }
